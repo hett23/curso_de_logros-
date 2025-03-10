807a737bb49889ba37f236b1b5e683717962a85e
hett23 <heberthrondon5@gmail.com>
2025-03-10 12:02:55 -0400

    Add files via upload

---
+++ archivo_2 (1).py
@@ -0,0 +1,13 @@
+rum= 15
+rum2= 23 
+suma= rum + rum2
+restar= rum - rum2 
+multiplicacion= rum * rum2
+divivion= rum / rum2
+print(F"la suma es:",suma)
+print(F"la resta es:",restar)
+print(F"la multiplicacion:", multiplicacion)
+print(F"la divivion:",divivion)
+
+
+
